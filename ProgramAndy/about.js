839366f20908c42fbc652d468e36f5c345c24cf1
lewieville <lewis.miller@svu.edu>
2024-12-29 21:50:50 -0500

    Delete ProgramAndy directory

--- ProgramAndy/about.js
+++
@@ -1,4 +0,0 @@
-// Example JavaScript for interactivity (optional)
-document.querySelector('.btn-primary').addEventListener('click', () => {
-    alert('Redirecting to Andy Crypto’s blog...');
-});
--- ProgramAndy/toc.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("agree-btn").addEventListener("click", function() {
-    alert("You have agreed to the terms of service. Thank you!");
-    window.location.href = "index.html"; // Redirect to another page after agreement
-});
-
-document.getElementById("disagree-btn").addEventListener("click", function() {
-    alert("You must agree to the terms of service to continue.");
-});
